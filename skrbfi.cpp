560c068188ce6a35cce7b6554986bfe5fdd87103
krzyszt0f8 <krzysztofdudek1232@gmail.com>
2023-02-22 23:15:04 +0100

    Switch instruction added

--- skrbfi.cpp
+++ skrbfi.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 //kolejno: liczba zestawów danych, liczba wskazówek, kierunek(0-3), ilosc kroków
-int d,n,a,b;
+int d,n;
+
+//współrzędne studni na osi XY
+constexpr int well_x = 0;
+constexpr int well_y = 0; 
 
 /*legenda:
     0 - północ
@@ -12,18 +16,46 @@ int d,n,a,b;
 */
 main ()
 {
+    cout << "Enter no of data sets: " << endl;
     cin >> d;
 
     for (int i = 0;i<d;i++)
-    {
+    {   
+        //obecne wsółrzędne skarbu 
+        int tresure_x = 0;
+        int tresure_y = 0;
+
+        cout << "Enter no of hints: " << endl;
         cin >> n;
+
         for (int j = 0; j<n;j++)
         {
-            cin >> a >> b ; 
+            int a , b;
+            cout <<"Enter direction & no of steps: " << endl;
             //dane wejsciowe: jaki kierunek, ile kroków
+            cin >> a >> b; 
 
+            switch (a)
+            {
+                case 0:
+                tresure_y += b;
+                break;
 
+                case 1:
+                tresure_y -=b;
+                break;
+
+                case 2:
+                tresure_x -=b;
+                break;
+
+                case 3:
+                tresure_x +=b;
+                break;
+            }
+        
         }
+        cout<<tresure_x<<endl<<tresure_y;
     }
 
     return 0;
