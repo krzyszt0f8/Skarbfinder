06176741fd6ca9a66690159cbb902a20e05caef0
krzyszt0f8 <krzysztofdudek1232@gmail.com>
2023-02-22 15:44:59 +0100

    First commit

---
+++ skrbfi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//kolejno: liczba zestawów danych, liczba wskazówek, kierunek(0-3), ilosc kroków
+int d,n,a,b;
+
+/*legenda:
+    0 - północ
+    1 - południe
+    2 - zachód
+    3 - wschód
+*/
+main ()
+{
+    cin >> d;
+
+    for (int i = 0;i<d;i++)
+    {
+        cin >> n;
+        for (int j = 0; j<n;j++)
+        {
+            cin >> a >> b ; 
+            //dane wejsciowe: jaki kierunek, ile kroków
+
+
+        }
+    }
+
+    return 0;
+}
